63848afa35692d7f4504384785b744580024666f
DJ <konobeat@gmail.com>
2018-10-06 07:11:50 -0400

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import SearchBar from './Components/search';
 import Drinks from './pages/Drinks';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+  
 
 class App extends Component {
  
